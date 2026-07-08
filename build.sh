c4fbbee3710597e7f07b0b72633c69f95aa0dee2
Congee <guswogus81@gmail.com>
2026-07-08 11:45:20 +0900

    Fix build.sh: copy dotfiles including .npmrc

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 cd ../
 mkdir output
 cp -R ./basic-project-apppang-18th-team3-frontend/* ./output
+cp -R ./basic-project-apppang-18th-team3-frontend/.[!.]* ./output
 cp -R ./output ./basic-project-apppang-18th-team3-frontend/
